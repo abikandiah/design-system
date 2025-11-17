4b3f659900bb7676b2a9bbbe3f4f00461e4acc91
abi <abikandiah@gmail.com>
2025-11-16 22:55:19 -0500

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 4
+}
+
+export default config
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import { withTV } from 'tailwind-variants'
+
+/** @type {import('tailwindcss').Config} */
+export default withTV({
+	darkMode: ["class"],
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'opac-white': 'white'
+			},
+			textShadow: {
+				'sm': '1px 1px 1px var(--tw-shadow-color)',
+				'md': '2px 2px 4px var(--tw-shadow-color)',
+				'lg': '4px 4px 8px var(--tw-shadow-color)',
+				'none': 'none',
+			},
+			listStyleType: {
+				square: 'square',
+				roman: 'upper-roman',
+			}
+		}
+	},
+	safelist: [
+		'opac-white',
+	],
+})
