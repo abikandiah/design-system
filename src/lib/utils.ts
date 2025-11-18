277fff39c02d385be23e04168548961af7f40b9f
abi <abikandiah@gmail.com>
2025-11-17 22:49:27 -0500

    added util funcs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,13 @@ export function getRandomIntUpTo(max: number) {
 	return Math.floor(Math.random() * (max + 1));
 }
 
+export function stringToBoolean(str: string | null): boolean {
+	if (!str) return false;
+	return ["true", "1", "yes"].includes(str.trim().toLowerCase());
+}
+
+export function booleanToString(bool: boolean | null): string {
+	if (!bool) return false.toString();
+	return bool.toString();
+}
+
